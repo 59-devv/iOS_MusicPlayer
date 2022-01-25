18868df5087e3c0f977d8100a7ee702e336ebde5
59 <5.9.devv@gmail.com>
2022-01-26 04:26:19 +0900

    22.01.26 - 인터페이스 빌더의 객체를 코드와 연결

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK : IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("button tapped")
+    }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
+    
 }
 
