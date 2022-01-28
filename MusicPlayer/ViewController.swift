cd1401b5374eb48cd178211089964eac4cd44743
59 <5.9.devv@gmail.com>
2022-01-28 22:21:54 +0900

    음원 재생, 재생버튼 클릭 시 아이콘 변경 처리

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -6,19 +6,82 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
-    // MARK : IBOutlets
+    // MARK: - Properties
+    var player: AVAudioPlayer!
+    var timer: Timer!
+    
+    // MARK: IBOutlets
     @IBOutlet var playPauseButton: UIButton!
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var progressSlider: UISlider!
     
-    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        self.addViewsWithCode()
+        self.initializePlayer()
     }
+    
+    // MARK: - Methods
+    // MARK: Custom Method
+    
+    // 플레이어를 초기화 하는 메서드
+    func initializePlayer() {
+        
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else {
+            print("음원 파일 에셋을 가져올 수 없습니다")
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("플레이어 초기화 실패")
+            print("코드 : \(error.code), 메세지 : \(error.localizedDescription)")
+        }
+        
+        self.progressSlider.maximumValue = Float(self.player.duration)
+        self.progressSlider.minimumValue = 0
+        self.progressSlider.value = Float(self.player.currentTime)
+    }
+    
+    
+    // 라벨을 분,초,밀리초 단위로 업데이트 해주는 메서드
+    func updateTimeLabelText(time: TimeInterval) {
+        let minute: Int = Int(time / 60)
+        let second: Int = Int(time.truncatingRemainder(dividingBy: 60))
+        let milisecond: Int = Int(time.truncatingRemainder(dividingBy: 1) * 100)
+        
+        let timeText: String = String(format: "%02ld:%02ld:%02ld", minute, second, milisecond)
+        
+        self.timeLabel.text = timeText
+    }
+    
+    
+    // 타이머를 만들고 수행해 줄 메서드
+    func makeAndFireTimer() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [unowned self] (timer: Timer) in
+          
+            if self.progressSlider.isTracking { return }
+            
+            self.updateTimeLabelText(time: self.player.currentTime)
+            self.progressSlider.value = Float(self.player.currentTime)
+        })
+        self.timer.fire()
+    }
+    
+    
+    // 타이머를 해제해 줄 메서드
+    func invalidateTimer() {
+        self.timer.invalidate()
+        self.timer = nil
+    }
+        
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -26,12 +89,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
-        print("button tapped")
+        sender.isSelected = !sender.isSelected
+                
+                if sender.isSelected {
+                    self.player?.play()
+                } else {
+                    self.player?.pause()
+                }
+                
+                if sender.isSelected {
+                    self.makeAndFireTimer()
+                } else {
+                    self.invalidateTimer()
+                }
     }
 
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        print("slider value changed")
-    }
+            self.updateTimeLabelText(time: TimeInterval(sender.value))
+            if sender.isTracking { return }
+            self.player.currentTime = TimeInterval(sender.value)
+        }
     
-}
+    
+    // 재생이 종료되었을 때, 다시 재생버튼이 일시정지 -> 재생으로 바꿈
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.playPauseButton.isSelected = false
+        self.progressSlider.value = 0
+        self.updateTimeLabelText(time: 0)
+        self.invalidateTimer()
+    }
 
+}
